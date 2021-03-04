5dfbce620a625e032437e49f37f5b7d1e4c5aa0f
jp919 <79021046+jp919@users.noreply.github.com>
2021-03-04 00:00:35 -0500

    Add files via upload
    This is a python based game code and stillin development

---
+++ car game finished.py
@@ -0,0 +1,245 @@
+#C:/Users/patri/OneDrive/Pictures/car draft #1.xcf
+#300x168
+ #("C:/Users/patri/OneDrive/Pictures/car-top-view-vehicle-overhead-260nw-1251380068.xcf").convert()
+#("C:/Users/patri/OneDrive/Pictures/ball2.xcf").convert()
+#color = (255, 231, 207)
+#pygame.mixer.music.load('C:/Users/patri/Music/Anthem - The Grand Affair.wav')
+#pygame.mixer.music.play(-1)
+#C:/Users/patri/OneDrive/Pictures/car #3.xcf
+import pygame
+import time
+import random
+ 
+pygame.init()
+
+#############
+crash_sound = pygame.mixer.Sound("C:/Users/patri/Music/Metal Crash.wav")
+#############
+color2=(180, 197, 247)
+display_width = 900
+display_height = 700
+color = (255, 231, 207)
+black = (0,0,0)
+white = (255,255,255)
+red = (200,0,0)
+green = (0,200,0)
+
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+ 
+block_color = (0,0,0)
+ 
+car_width = 73
+ 
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('crazy race')
+clock = pygame.time.Clock()
+ 
+carImg = pygame.image.load('C:/Users/patri/OneDrive/Pictures/car #3.xcf')
+background_image = pygame.image.load("C:/Users/patri/OneDrive/Pictures/road 2.xcf").convert()
+background_image2 = pygame.image.load("C:/Users/patri/OneDrive/Pictures/IMG-0593.xcf").convert()
+
+
+                        
+pause = False
+#crash = True
+ 
+def things_dodged(count):
+    font = pygame.font.SysFont("comicsansms", 25)
+    text = font.render("Dodged: "+str(count), True, white)
+    gameDisplay.blit(text,(0,0))
+ 
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+ 
+def car(x,y):
+    gameDisplay.blit(carImg,(x,y))
+ 
+def text_objects(text, font):
+    textSurface = font.render(text, True, white)
+    return textSurface, textSurface.get_rect()
+ 
+ 
+def crash():
+    ####################################
+    pygame.mixer.Sound.play(crash_sound)
+    pygame.mixer.music.stop()
+    ####################################
+    largeText = pygame.font.SysFont("comicsansms",115)
+    TextSurf, TextRect = text_objects("You Crashed", largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+    
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        
+
+        button("Play Again",150,450,100,50,green,bright_green,game_loop)
+        button("Quit",550,450,100,50,red,bright_red,quitgame)
+
+        pygame.display.update()
+        clock.tick(15) 
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+        if click[0] == 1 and action != None:
+            action()         
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
+    
+
+def quitgame():
+    pygame.quit()
+    quit()
+
+def unpause():
+    global pause
+    pygame.mixer.music.unpause()
+    pause = False
+    
+
+def paused():
+    ############
+    pygame.mixer.music.pause()
+    #############
+    largeText = pygame.font.SysFont("comicsansms",115)
+    TextSurf, TextRect = text_objects("Paused", largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+    
+
+    while pause:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+
+        button("Continue",150,450,100,50,green,bright_green,unpause)
+        button("Quit",550,450,100,50,red,bright_red,quitgame)
+
+        pygame.display.update()
+        clock.tick(15)   
+
+
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            #print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        gameDisplay.blit(background_image2, [0, 0])
+        largeText = pygame.font.SysFont("comicsansms",115)
+        TextSurf, TextRect = text_objects("crazy race", largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        button("GO!",150,450,100,50,green,bright_green,game_loop)
+        button("Quit",550,450,100,50,red,bright_red,quitgame)
+
+        pygame.display.update()
+        clock.tick(15)
+        
+        
+    
+    
+
+    
+def game_loop():
+    global pause
+    ############
+    pygame.mixer.music.load('C:/Users/patri/Music/Anthem - The Grand Affair.wav')
+    pygame.mixer.music.play(-1)
+    ############
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+ 
+    x_change = 0
+ 
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 4
+    thing_width = 100
+    thing_height = 100
+ 
+    thingCount = 1
+ 
+    dodged = 0
+ 
+    gameExit = False
+ 
+    while not gameExit:
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+ 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -10
+                if event.key == pygame.K_RIGHT:
+                    x_change = 10
+                if event.key == pygame.K_p:
+                    pause = True
+                    paused()
+                    
+ 
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+ 
+        x += x_change
+        
+        gameDisplay.blit(background_image, [0, 0])
+        things(thing_startx, thing_starty, thing_width, thing_height, block_color)
+ 
+       
+        
+        thing_starty += thing_speed
+        car(x,y)
+        things_dodged(dodged)
+ 
+        if x > display_width - car_width or x < 0:
+            crash()
+ 
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0,display_width)
+            dodged += 1
+            thing_speed += 1
+            thing_width += (dodged * 1.2)
+ 
+        if y < thing_starty+thing_height:
+            print('y crossover')
+ 
+            if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx+thing_width:
+                print('x crossover')
+                crash()
+        
+        pygame.display.update()
+        clock.tick(60)
+
+game_intro()
+game_loop()
+pygame.quit()
+quit()
+
